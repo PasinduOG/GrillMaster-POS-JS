9f041c1750285c3554976d64a0ec170b0716b052
Pasindu Madhuwantha <126347762+PasinduOG@users.noreply.github.com>
2025-11-26 16:52:53 +0530

    Enhance user experience by adding AOS animations, updating login functionality with spinner and alert messages, and modifying customer data for consistency.

--- js/index.js
+++ js/index.js
@@ -44,30 +44,69 @@ function updateNav() {
 function loadComponent(name) {
     fetch(`components/${name}/${name}.html`)
         .then(res => res.text())
-        .then(html => root.innerHTML = html);
+        .then(html => {
+            root.innerHTML = html;
+            let userName = document.getElementById("userId");
+            let userIdLabel = document.getElementById("userIdLabel");
+            if (customerData.length != 0) {
+                userName.innerText = customerData[0].name;
+                userIdLabel.classList.remove("d-none");
+            } else {
+                userIdLabel.classList.add("d-none");
+            }
+        });
 }
 
 function checkLogin() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    let spinner = document.getElementById("loginSpinner");
+    spinner.classList.remove("d-none");
+
     fetch("../json/customer.json")
         .then(res => res.json())
         .then(data => {
             data.forEach(customer => {
                 if (customer.email == email && customer.password == password) {
+                    spinner.classList.add("d-none");
                     customerData.push(customer);
                     updateNav();
                     loadComponent('home');
+                } else {
+                    spinner.classList.add("d-none");
+                    document.getElementById("alertMessage").classList.remove("d-none");
+                    setTimeout(() => {
+                        document.getElementById("alertMessage").classList.add("d-none");
+                    }, 3000);
                 }
             });
         });
 }
 
-function logOut(){
-    customerData = [];
-    updateNav();
-    loadComponent("login");
+function logOut() {
+    Swal.fire({
+        title: "Are you sure you want to log out?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Yes, log out!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Logged out!",
+                text: "Please login to the application again.",
+                icon: "success"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    customerData = [];
+                    updateNav();
+                    loadComponent("login");
+                }
+            });
+        }
+    });
 }
 
 window.loadComponent = loadComponent;
